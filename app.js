6fd72bc6e14938f36fe879a379085a31322fa42f
ilyusha71 <ilyusha71@gmail.com>
2016-03-31 20:54:29 +0800

    Added files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var wakakaGalaxyInfo = 
+{
+	title:['оди́н', 'два́', 'три́', 'четы́ре', 'пя́ть', 'ше́сть', 'се́мь', 'во́семь', 'де́вять', 'де́сять', 'оди́н на дцать', 'две на́ дцать'],
+	imgPath:['wakaka01','wakaka02','wakaka03','wakaka04','wakaka05','wakaka06','wakaka07','wakaka08','wakaka09','wakaka10','wakaka11','wakaka12']
+}
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', function (req, res, next) 
+{
+	console.log('Request Url:' + req.url);
+	next();
+}
+);
+
+/* EJS -> view folder */
+app.set('view engine', 'ejs'); 
+
+/* index.ejs -> index.html */
+app.get('/', function(req, res) 
+{
+	res.render('index',wakakaGalaxyInfo);
+}
+);
+
+app.listen(port);
